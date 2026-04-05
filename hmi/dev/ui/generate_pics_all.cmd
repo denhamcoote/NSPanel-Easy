c3e990b24d7bfeb1abc7dbdff2de9c53bec874ab
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-04-05 13:46:50 +0200

    new clear night pic
    #66

--- hmi/dev/ui/generate_pics_all.cmd
+++ hmi/dev/ui/generate_pics_all.cmd
@@ -1,6 +1,12 @@
 bash ../scripts/generate_images.sh pics/0.png eu
+if errorlevel 1 timeout /t 10
 bash ../scripts/generate_images.sh pics/0-portrait.png us
+if errorlevel 1 timeout /t 10
 bash ../scripts/generate_images.sh pics/0-white.png eu-white
+if errorlevel 1 timeout /t 10
 bash ../scripts/generate_images.sh pics/0-portrait-white.png us-white
+if errorlevel 1 timeout /t 10
+ 
+timeout /t 30
 
 
